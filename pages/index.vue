7404521c3366a314643de9e6ce2df358a08dbe25
mekan <gitmekan13@gmail.com>
2024-10-27 10:33:16 +0800

    feat: add landing page

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="mt-14 grid grid-cols-5 gap-5 p-2">
+        <Litty/>
+        <Profile  class="col-span-3"/>
+    </div>
+</template>
