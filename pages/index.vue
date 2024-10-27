766a45c284f64a77864a96b8c73961eea9c7c6c4
mekan <gitmekan13@gmail.com>
2024-10-27 13:18:46 +0800

    feat: import components to grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+definePageMeta({
+  colorMode: 'dark',
+})
 </script>
 
 <template>
-    <div class="mt-14 grid grid-cols-5 gap-5 p-2">
-        <Litty/>
-        <Profile  class="col-span-3"/>
+    <div class="mt-14 grid grid-cols-8 gap-5 p-2">
+        <Litty class="col-span-2" />
+        <Profile  class="col-span-4"/>
+        <Match class="col-span-2" />
+        <div class="col-span-2">
+            <Trends />
+        </div>
+        <MainMenu class="col-span-4" />
+        <div class="col-span-2">
+            <Events />
+        </div>
     </div>
 </template>
